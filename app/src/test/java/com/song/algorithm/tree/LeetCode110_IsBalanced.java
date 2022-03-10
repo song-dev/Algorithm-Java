7ccbfbdf4a890efb6658d006c3e95f23e3a369e1
chensongsong <chensongsong@geetest.com>
2022-03-10 11:57:18 +0800

    [LeetCode-110] 平衡二叉树

---
+++ app/src/test/java/com/song/algorithm/tree/LeetCode110_IsBalanced.java
@@ -0,0 +1,99 @@
+package com.song.algorithm.tree;
+
+import org.junit.Test;
+
+/**
+ * 110. 平衡二叉树
+ * https://leetcode-cn.com/problems/balanced-binary-tree
+ * <p>
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * <p>
+ * 本题中，一棵高度平衡二叉树定义为：
+ * <p>
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ * 示例 3：
+ * <p>
+ * 输入：root = []
+ * 输出：true
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 树中的节点数在范围 [0, 5000] 内
+ * -104 <= Node.val <= 104
+ */
+public class LeetCode110_IsBalanced {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 自底向上递归
+     *
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+        System.out.println(isBalanced(root));
+    }
+}
