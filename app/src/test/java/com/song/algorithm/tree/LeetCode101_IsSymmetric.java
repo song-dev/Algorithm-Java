a4407b8518bc53c25b9430e2d40d4d084f0a33e9
chensongsong <chensongsong@geetest.com>
2022-03-10 11:57:52 +0800

    [LeetCode-101] 对称二叉树

---
+++ app/src/test/java/com/song/algorithm/tree/LeetCode101_IsSymmetric.java
@@ -0,0 +1,111 @@
+package com.song.algorithm.tree;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 101. 对称二叉树
+ * https://leetcode-cn.com/problems/symmetric-tree
+ * <p>
+ * 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点数目在范围 [1, 1000] 内
+ * -100 <= Node.val <= 100
+ * <p>
+ * 进阶：你可以运用递归和迭代两种方法解决这个问题吗？
+ */
+public class LeetCode101_IsSymmetric {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 类似双指针实现
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if (left == null && right == null) {
+                continue;
+            }
+            if ((left == null || right == null) || left.val != right.val) {
+                return false;
+            }
+            queue.offer(left.left);
+            queue.offer(right.right);
+
+            queue.offer(left.right);
+            queue.offer(right.left);
+        }
+        return true;
+    }
+
+    public boolean isSymmetric2(TreeNode root) {
+        return check(root, root);
+    }
+
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(2);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(5);
+        TreeNode node7 = new TreeNode(4);
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+        System.out.println(isSymmetric(root));
+        System.out.println(isSymmetric2(root));
+    }
+}
