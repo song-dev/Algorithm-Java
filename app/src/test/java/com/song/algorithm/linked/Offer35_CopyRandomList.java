c3eae53b68dfac990f662e390c5e3f41ec8fa97c
chensongsong <chensongsong@geetest.com>
2022-03-10 18:26:31 +0800

    [Offer-35] 复杂链表的复制

---
+++ app/src/test/java/com/song/algorithm/linked/Offer35_CopyRandomList.java
@@ -0,0 +1,103 @@
+package com.song.algorithm.linked;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 剑指 Offer 35. 复杂链表的复制
+ * https://leetcode-cn.com/problems/fu-za-lian-biao-de-fu-zhi-lcof/
+ * <p>
+ * 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 示例 2：
+ * <p>
+ * 输入：head = [[1,1],[2,1]]
+ * 输出：[[1,1],[2,1]]
+ * 示例 3：
+ * <p>
+ * 输入：head = [[3,null],[3,0],[3,null]]
+ * 输出：[[3,null],[3,0],[3,null]]
+ * 示例 4：
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ * 解释：给定的链表为空（空指针），因此返回 null。
+ *  
+ * 提示：
+ * <p>
+ * -10000 <= Node.val <= 10000
+ * Node.random 为空（null）或指向链表中的节点。
+ * 节点数目不超过 1000 。
+ */
+public class Offer35_CopyRandomList {
+
+    static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    Map<Node, Node> cachedNode = new HashMap<>();
+
+    /**
+     * 回溯+哈希表
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(n)
+     *
+     * @param head
+     * @return
+     */
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        if (!cachedNode.containsKey(head)) {
+            Node headNew = new Node(head.val);
+            cachedNode.put(head, headNew);
+            headNew.next = copyRandomList(head.next);
+            headNew.random = copyRandomList(head.random);
+        }
+        return cachedNode.get(head);
+    }
+
+    /**
+     * 迭代+节点拆分
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(1)
+     *
+     * @param head
+     * @return
+     */
+    public Node copyRandomList2(Node head) {
+        if (head == null) {
+            return null;
+        }
+        for (Node node = head; node != null; node = node.next.next) {
+            Node nodeNew = new Node(node.val);
+            nodeNew.next = node.next;
+            node.next = nodeNew;
+        }
+        for (Node node = head; node != null; node = node.next.next) {
+            Node nodeNew = node.next;
+            nodeNew.random = (node.random != null) ? node.random.next : null;
+        }
+        Node headNew = head.next;
+        for (Node node = head; node != null; node = node.next) {
+            Node nodeNew = node.next;
+            node.next = node.next.next;
+            nodeNew.next = (nodeNew.next != null) ? nodeNew.next.next : null;
+        }
+        return headNew;
+    }
+
+}
