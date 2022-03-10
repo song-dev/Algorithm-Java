600d5ba2025fc6713b7c339787bfbb75f55cf4f9
chensongsong <chensongsong@geetest.com>
2022-03-10 11:59:15 +0800

    [LeetCode-222] 完全二叉树的节点个数

---
+++ app/src/test/java/com/song/algorithm/tree/LeetCode222_CountNodes.java
@@ -0,0 +1,136 @@
+package com.song.algorithm.tree;
+
+import org.junit.Test;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 222. 完全二叉树的节点个数
+ * https://leetcode-cn.com/problems/count-complete-tree-nodes
+ *
+ * 给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+ *
+ * 完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+ *
+ * 示例 1：
+ *
+ * 输入：root = [1,2,3,4,5,6]
+ * 输出：6
+ * 示例 2：
+ *
+ * 输入：root = []
+ * 输出：0
+ * 示例 3：
+ *
+ * 输入：root = [1]
+ * 输出：1
+ *
+ * 提示：
+ *
+ * 树中节点的数目范围是[0, 5 * 104]
+ * 0 <= Node.val <= 5 * 104
+ * 题目数据保证输入的树是 完全二叉树
+ *  
+ *
+ * 进阶：遍历树来统计节点是一种时间复杂度为 O(n) 的简单解决方案。你可以设计一个更快的算法吗？
+ */
+public class LeetCode222_CountNodes {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 0;
+        TreeNode node = root;
+        while (node.left != null) {
+            level++;
+            node = node.left;
+        }
+        int low = 1 << level, high = (1 << (level + 1)) - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (exists(root, level, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean exists(TreeNode root, int level, int k) {
+        int bits = 1 << (level - 1);
+        TreeNode node = root;
+        while (node != null && bits > 0) {
+            if ((bits & k) == 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+            bits >>= 1;
+        }
+        return node != null;
+    }
+
+    /**
+     * 前序遍历迭代实现
+     *
+     * @param root
+     * @return
+     */
+    public int countNodes2(TreeNode root) {
+        int count = 0;
+        Deque<TreeNode> queue = new LinkedList<>();
+        TreeNode cur = root;
+        while (cur != null || !queue.isEmpty()) {
+            if (cur != null) {
+                queue.push(cur);
+                cur = cur.left;
+                count++;
+            } else {
+                cur = queue.pop();
+                cur = cur.right;
+            }
+        }
+        return count;
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+        System.out.println(countNodes(root));
+        System.out.println(countNodes2(root));
+    }
+}
