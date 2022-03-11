ad30f362abc8125f6537d74c98bf2e826e0907f7
chensongsong <chensongsong@geetest.com>
2022-03-11 09:58:05 +0800

    [Offer-18] 删除链表的节点

---
+++ app/src/test/java/com/song/algorithm/linked/Offer18_DeleteNode.java
@@ -0,0 +1,82 @@
+package com.song.algorithm.linked;
+
+import org.junit.Test;
+
+/**
+ * 剑指 Offer 18. 删除链表的节点
+ * https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof
+ * <p>
+ * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+ * <p>
+ * 返回删除后的链表的头节点。
+ * <p>
+ * 注意：此题对比原题有改动
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: head = [4,5,1,9], val = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ * 示例 2:
+ * <p>
+ * 输入: head = [4,5,1,9], val = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ *  
+ * 说明：
+ * <p>
+ * 题目保证链表中节点的值互不相同
+ * 若使用 C 或 C++ 语言，你不需要 free 或 delete 被删除的节点
+ */
+public class Offer18_DeleteNode {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    /**
+     * 双指针
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode deleteNode(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        if (head.val == val) {
+            return head.next;
+        }
+        ListNode pre = head, cur = head.next;
+        while (cur != null && cur.val != val) {
+            pre = cur;
+            cur = cur.next;
+        }
+        if (cur != null) {
+            pre.next = cur.next;
+        }
+        return head;
+    }
+
+    @Test
+    public void test() {
+        ListNode node = new ListNode(0);
+        node.next = new ListNode(1);
+        node.next.next = new ListNode(2);
+        node.next.next.next = new ListNode(3);
+        node.next.next.next.next = new ListNode(4);
+        node.next.next.next.next.next = new ListNode(5);
+        ListNode root = deleteNode(node, 4);
+        while (root != null) {
+            System.out.println(root.val);
+            root = root.next;
+        }
+    }
+}
