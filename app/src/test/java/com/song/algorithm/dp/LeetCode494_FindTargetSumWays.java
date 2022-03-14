0f029b870aae9326163ce405fd5cf83bcd0b9039
chensongsong <chensongsong@geetest.com>
2022-03-14 14:15:00 +0800

    [LeetCode-494] 目标和

---
+++ app/src/test/java/com/song/algorithm/dp/LeetCode494_FindTargetSumWays.java
@@ -0,0 +1,77 @@
+package com.song.algorithm.dp;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 494. 目标和
+ * https://leetcode-cn.com/problems/target-sum/
+ * <p>
+ * 给你一个整数数组 nums 和一个整数 target 。
+ * <p>
+ * 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+ * <p>
+ * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+ * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,1,1,1,1], target = 3
+ * 输出：5
+ * 解释：一共有 5 种方法让最终目标和为 3 。
+ * -1 + 1 + 1 + 1 + 1 = 3
+ * +1 - 1 + 1 + 1 + 1 = 3
+ * +1 + 1 - 1 + 1 + 1 = 3
+ * +1 + 1 + 1 - 1 + 1 = 3
+ * +1 + 1 + 1 + 1 - 1 = 3
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1], target = 1
+ * 输出：1
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 20
+ * 0 <= nums[i] <= 1000
+ * 0 <= sum(nums[i]) <= 1000
+ * -1000 <= target <= 1000
+ */
+public class LeetCode494_FindTargetSumWays {
+
+    /**
+     * 1. dp[i] 表示 i 位置存在 dp[i] 种可能性等于 (sum+target)/2
+     * 2. dp[j] += dp[j - nums[i]]
+     * 3. 初始化值 0 为 1
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if ((target + sum) % 2 != 0) {
+            return 0;
+        }
+        int size = (target + sum) / 2;
+        if (size < 0) size = -size;
+        int[] dp = new int[size + 1];
+        dp[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = size; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+            System.out.println(Arrays.toString(dp));
+        }
+        return dp[size];
+    }
+
+    @Test
+    public void test() {
+        System.out.println(findTargetSumWays(new int[]{1, 1, 1, 1, 1}, 3));
+    }
+
+}
