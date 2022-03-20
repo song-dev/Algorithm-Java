a10c6e71604fc617a3048b4a7c78f0ef0136130c
chensongsong <chensongsong@geetest.com>
2022-03-20 21:02:21 +0800

    [LeetCode-46] 全排列

---
+++ app/src/test/java/com/song/algorithm/backtracking/LeetCode46_Permute.java
@@ -0,0 +1,71 @@
+package com.song.algorithm.backtracking;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 46. 全排列
+ * https://leetcode-cn.com/problems/permutations/
+ * <p>
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1]
+ * 输出：[[1]]
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * nums 中的所有整数 互不相同
+ */
+public class LeetCode46_Permute {
+
+    public List<List<Integer>> permute(int[] nums) {
+        result.clear();
+        boolean[] used = new boolean[nums.length];
+        backtracking(nums, used);
+        return result;
+    }
+
+    private final List<List<Integer>> result = new ArrayList<>();
+    private final LinkedList<Integer> path = new LinkedList<>();
+
+    public void backtracking(int[] nums, boolean[] used) {
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            used[i] = true;
+            path.add(nums[i]);
+            backtracking(nums, used);
+            path.removeLast();
+            used[i] = false;
+        }
+    }
+
+    @Test
+    public void test() {
+        for (List<Integer> item : permute(new int[]{4, 6, 7})) {
+            System.out.println(Arrays.toString(item.toArray()));
+        }
+    }
+
+}
